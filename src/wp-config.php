29553cba8c8e995cec0a44e4ee3f05e650b53a77
Elvis Martins <elvisbmartins@gmail.com>
2017-02-09 23:02:25 -0200

    wp-config add wp_lang pt_br

--- src/wp-config.php
+++ src/wp-config.php
@@ -38,6 +38,9 @@ define('DB_CHARSET', 'utf8mb4');
 /** O tipo de Collate do banco de dados. Não altere isso se tiver dúvidas. */
 define('DB_COLLATE', '');
 
+
+define('WP_LANG', 'pt_BR');
+
 /**#@+
  * Chaves únicas de autenticação e salts.
  *
